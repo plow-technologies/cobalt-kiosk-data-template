9d09abda414bd5b1fa2c6b7199ec75153efc5d9e
Scott Murphy <scottmurphy09@gmail.com>
2015-02-03 17:51:48 -0600

    Moving Migrations

--- src/Kiosk/Backend/Data/Migrations.hs
+++ src/Kiosk/Backend/Data/Migrations.hs
@@ -80,20 +80,6 @@ data FormVersionZero = FormVersionZero { _signature_1                 :: Text
                                        } deriving (Show)
 
                                        
--- | Form Sample
--- BBLS:_Produced_Water_1: "45"
--- Date_1: "1-26-15"
--- Driver_Signature_1: ""
--- Flowback_Water_1: ""
--- Fresh_Water_1: ""
--- Lease_Name_1: "H-2 plant"
--- Name_of_Lease_Operator_1: "Citation"
--- Name_of_Water_Hauling_Company_1: "Brady inc"
--- Pit_Water_1: ""
--- Time_in_1: ""
--- Truck_#_1: "31"
--- Water_Hauling_Permit_#_1: ""
--- signature_1: "/9j/4AAQSkZJRgABAQAASABIAAD/4QBYRXhpZgAATU0AKgAAAAgAAgESAAMAAAABAAEAAIdpAAQAAAABAAAAJgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAACZ6ADAAQAAAABAAAAfQAAAAD/7QA4UGhvdG9zaG9wIDMuMAA4QklNBAQAAAAAAAA4QklNBCU
 
 
 toFormVersionZeroEntry :: DataTemplateEntry -> FormVersionZeroEntry
