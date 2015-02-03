579009caefd9c564e97e69d99af20e5b684069ff
Scott Murphy <scottmurphy09@gmail.com>
2015-02-03 17:52:06 -0600

    Moving different Migration pieces to their respective spaces so that you know, it doesn't suck so much

---
+++ src/Kiosk/Backend/Data/Migrations/FormVersionZero.hs
@@ -0,0 +1,35 @@
+
+
+{- |
+Module      :  Kiosk.Backend.Data.Migrations.FormVersionZero.hs
+Description :  First Version of the Kiosk Form, 
+Copyright   :  Plow Technologies LLC 
+License     :  MIT License
+
+Maintainer  :  Scott Murphy
+Stability   :  experimental
+Portability :  portable
+
+-- | Form Sample
+-- BBLS:_Produced_Water_1: "45"
+-- Date_1: "1-26-15"
+-- Driver_Signature_1: ""
+-- Flowback_Water_1: ""
+-- Fresh_Water_1: ""
+-- Lease_Name_1: "H-2 plant"
+-- Name_of_Lease_Operator_1: "Citation"
+-- Name_of_Water_Hauling_Company_1: "Brady inc"
+-- Pit_Water_1: ""
+-- Time_in_1: ""
+-- Truck_#_1: "31"
+-- Water_Hauling_Permit_#_1: ""
+-- signature_1: "/9j/4AAQSkZJRgABAQAASABIAAD/4QBYRXhpZgAATU0AKgAAAAgAAgESAAMAAAABAAEAAIdpAAQAAAABAAAAJgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAACZ6ADAAQAAAABAAAAfQAAAAD/7QA4UGhvdG9zaG9wIDMuMAA4QklNBAQAAAAAAAA4QklNBCU
+   
+   
+
+-}
+
+
+module Kiosk.Backend.Data.Migrations.FormVersionZero () where
+
+
