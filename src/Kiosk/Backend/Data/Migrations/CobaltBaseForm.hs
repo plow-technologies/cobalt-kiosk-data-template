1e0703169724360fe007435f963f6d257e4d5648
Scott Murphy <scottmurphy09@gmail.com>
2015-02-03 18:38:07 -0600

    Added base form

---
+++ src/Kiosk/Backend/Data/Migrations/CobaltBaseForm.hs
@@ -0,0 +1,17 @@
+
+
+{- |
+Module      :  Kiosk.Backend.Data.Migrations.CobaltBaseForm
+Description :  Base Form for all company forms to follow
+Copyright   :  Plow Technologies LLC 
+License     :  MIT License
+
+Maintainer  :  Scott Murphy
+Stability   :  experimental
+Portability :  portable
+
+Basically, I don't want to do the work of changing every single form to match the company so we are going to have one form that will be translated into the various company forms
+
+-}
+
+module Kiosk.Backend.Data.Migrations.CobaltBaseForm () where        
