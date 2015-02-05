6d00799a3b635f8df32877d287411c21f05cbdaf
Scott Murphy <scottmurphy09@gmail.com>
2015-02-04 22:14:13 -0600

    Updated Tests

--- test/Kiosk/Backend/Data/MigrationsSpec.hs
+++ test/Kiosk/Backend/Data/MigrationsSpec.hs
@@ -8,8 +8,15 @@ import           Language.Haskell.TH
 import           Test.Hspec
 -- import           Test.HUnit
 
-import           Kiosk.Backend.Data            (DataTemplateEntry (..))
-import           Kiosk.Backend.Data.Migrations (FormVersionOneEntry (..),
+import Kiosk.Backend.Data.DataTemplate (TemplateItem(..))
+import Kiosk.Backend.Form
+import Data.UUID
+import           Kiosk.Backend.Data            (DataTemplateEntry (..)
+                                               ,DataTemplate(..)
+                                               
+                                               ,TicketId(..)
+                                               ,DataTemplateEntryKey(..))
+import           Kiosk.Backend.Data.Migrations ( CobaltBaseFormEntry (..),
                                                 FormVersionZeroEntry (..) )
 import Data.Text                                                
 import Control.Applicative 
@@ -23,10 +30,12 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe (nameBase 'FormVersionOneEntry) $
-   it "Should convert FormVersionZeroEntry to FormVersionOneEntry" $ do
+  describe (nameBase 'CobaltBaseFormEntry) $
+   it ("Should convert " ++ (nameBase 'CobaltBaseFormEntry) ++ "to" ++ (nameBase 'CobaltBaseFormEntry)) $ do
      let (Right dt) = eitherDecode testFormVersionZeroDataTemplateEntry :: Either String DataTemplateEntry
-         (Success fv1) = (transformRecord . fromDataTemplate $ dt) :: Validation (MigrationError Text FormVersionZeroEntry) FormVersionOneEntry
+         (Success fv1) = (transformRecord . fromDataTemplate $ dt) :: Validation (MigrationError Text FormVersionZeroEntry) CobaltBaseFormEntry
          backToDataTemplateEntry :: DataTemplateEntry
          backToDataTemplateEntry = toDataTemplate fv1
-     True `shouldBe` True
+     (encode $ backToDataTemplateEntry) `shouldBe` "{\"value\":{\"Name_of_Water_Hauling_Company\":\"Brady inc\",\"Amount\":25,\"Type_of_Water_Hauled\":\"Produced Water\",\"Customer_Ticket_Number\":\"\",\"Lease_Name\":\"H-5 oil batt\",\"Date\":\"1-26-15\",\"Name_of_Lease_Operator\":\"Citation\",\"Time_in\":\"\",\"Driver_Signature\":\"test_signature\",\"Water_Hauling_Permit#\":\"\"},\"key\":{\"uuid\":\"a2e3609e-154d-4e60-80e0-c77189098617\",\"ticketid\":\"1-1245\",\"date\":\"1422308331184\",\"formid\":\"1\"}}"
+
+
